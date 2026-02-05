b63d426fda70770fb04a6501a18e96a7e3850f62
Tood <116191390+Le-Tim@users.noreply.github.com>
2026-02-05 01:32:18 -0500

    model imports and enemy spawning script

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private float spawnInterval = 3.5f;
+    void Start()
+    {
+        StartCoroutine(SpawnEnemy(spawnInterval, enemyPrefab));
+    }
+    private IEnumerator SpawnEnemy(float interval, GameObject enemy)
+    {
+        yield return new WaitForSeconds(interval);
+        Vector3 spawnPos = new Vector3(
+            Random.Range(-5f, 5f), 0f, Random.Range(-6f, 6f)
+        );
+        Instantiate(enemy, spawnPos, Quaternion.identity);
+        StartCoroutine(SpawnEnemy(interval, enemy));
+    }
+}
